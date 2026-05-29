40fbc3db0b44bfd70c92f5951d1979d1a7ad3444
anamwagdarikar-eng <anam.wagdarikar@gmail.com>
2026-05-29 11:14:49 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
-# Utils Package
-# Individual modules are imported on demand by main application
+# MCQ Test System Package
+__version__ = "1.0.0"
+__author__ = "Engineering College"
+__description__ = "Online MCQ Test System for Engineering Colleges"
